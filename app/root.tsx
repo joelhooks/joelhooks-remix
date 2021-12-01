484406b045d4679de44adc11e92e394207963463
Joel Hooks <joelhooks@gmail.com>
2021-11-30 22:04:51 -0800

    rename folder

--- app/root.tsx
+++ app/root.tsx
@@ -104,6 +104,39 @@ export type LoaderData = {
 // https://remix.run/api/app#links
 export let links: LinksFunction = () => {
   return [
+    {
+      rel: 'preload',
+      as: 'font',
+      href: '/fonts/Inter-UI-Bold.woff',
+      type: 'font/woff',
+      crossOrigin: 'anonymous',
+    },
+    {
+      rel: 'preload',
+      as: 'font',
+      href: '/fonts/Inter-UI-Regular.woff',
+      type: 'font/woff',
+      crossOrigin: 'anonymous',
+    },
+    {
+      rel: 'apple-touch-icon',
+      sizes: '180x180',
+      href: '/favicons/apple-touch-icon.png',
+    },
+    {
+      rel: 'icon',
+      type: 'image/png',
+      sizes: '32x32',
+      href: '/favicons/favicon-32x32.png',
+    },
+    {
+      rel: 'icon',
+      type: 'image/png',
+      sizes: '16x16',
+      href: '/favicons/favicon-16x16.png',
+    },
+    {rel: 'manifest', href: '/site.webmanifest'},
+    {rel: 'icon', href: '/favicon.ico'},
     {rel: 'stylesheet', href: vendorStyles},
     {rel: 'stylesheet', href: tailwindStyles},
     {rel: 'stylesheet', href: proseStyles},
